72257afe0a89e67b651ea07fc466d63d075fa43e
Priscila Di Renzo <prisciladirenzo@gmail.com>
2024-06-14 12:06:12 -0300

    ejercicio 1

---
+++ pooSolution/exercises/Entities/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercises
+{
+    public class Person
+    {
+
+        public string Name { get; set; }
+
+        // constructor
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string Introduce()
+        {
+            return $"Hola, mi nombre es {Name}!";
+        }
+
+
+    }
+}
---
+++ pooSolution/exercises/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exercises
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese nombre 1: ");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Ingrese nombre 2: ");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Ingrese nombre 3: ");
+            string name3 = Console.ReadLine();
+
+            List <Person> people = new List<Person>();
+
+            people.Add(new Person(name1));
+            people.Add(new Person(name2));  
+            people.Add(new Person(name3));
+
+            foreach(Person person in people)
+            {
+                Console.WriteLine(person.Introduce());
+            }
+
+        }
+    }
+}
